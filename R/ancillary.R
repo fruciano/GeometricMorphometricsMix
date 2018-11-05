aa04b43298a4733339593e7487e6e5fe8e164b99
Carmelo Fruciano <36262878+fruciano@users.noreply.github.com>
2018-11-05 16:12:13 +0100

    Added rotate_landmarks

--- R/ancillary.R
+++ R/ancillary.R
@@ -10,5 +10,8 @@ rep.col=function(x,n){
    matrix(rep(x,each=n), ncol=n, byrow=TRUE)
 }
 
-# Function to convert radians to degrees
+
+# Functions to convert radians to degrees
+# and degrees to radians
 rad2deg = function(rad) {(rad * 180) / (pi)}
+deg2rad = function(deg) {(deg * pi) / (180)}
